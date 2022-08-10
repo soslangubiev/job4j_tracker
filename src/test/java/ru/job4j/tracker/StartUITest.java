b18f2fb5d41e35606b15a21e5e895093d6d6552e
Soslan Gubiev <Gubievs91@gmail.com>
2022-08-10 19:39:32 +0500

    9. Написать тесты на StartUI. [#785] fix checkstyle issue

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -40,7 +40,7 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
-                new String[]{"0",String.valueOf(item.getId()), "1"}
+                new String[]{"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
